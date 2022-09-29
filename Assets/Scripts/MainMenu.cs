62ec213d341994bf780740a6b3d1e042aaac01e6
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-09-29 15:58:48 +0300

    fix old colourings and references from prev game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 
     public void NewGame()
     {
-        SceneManager.LoadScene(playGameScene);
+        // SceneManager.LoadScene(playGameScene);
         if (playGameScene == "Start")
         {
             Destroy (GameObject.Find("MainCamera"));
