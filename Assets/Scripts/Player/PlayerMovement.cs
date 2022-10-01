8131f1645c98d0d5992f468dd9bd859a3e6ad2db
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-10-01 14:43:13 +0300

    namespace updates

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,43 +1,45 @@
 using UnityEngine;
 
-
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    /*
+    public class PlayerMovement : MonoBehaviour
+    {
+        /*
      * Player movement speed.
      */
-    [Tooltip("How far in game units that the player can move per tick.")] [SerializeField]
-    public float movementSpeedInitial = 2F;
-    public float movementSpeedActual;
+        [Tooltip("How far in game units that the player can move per tick.")] [SerializeField]
+        public float movementSpeedInitial = 2F;
+        public float movementSpeedActual;
 
-    public Rigidbody2D rb;
-    public Animator animator;
+        public Rigidbody2D rb;
+        public Animator animator;
 
-    private Vector2 _movement;
+        private Vector2 _movement;
 
-    void Start()
-    {
-        movementSpeedActual = movementSpeedInitial;
+        void Start()
+        {
+            movementSpeedActual = movementSpeedInitial;
 
-        //If missing components
-        if (!rb) rb = GetComponent<Rigidbody2D>();
-        if (!animator) animator = GetComponent<Animator>();
+            //If missing components
+            if (!rb) rb = GetComponent<Rigidbody2D>();
+            if (!animator) animator = GetComponent<Animator>();
 
-    }
+        }
 
-    void Update()
-    {
-        _movement.x = Input.GetAxisRaw("Horizontal");
-        _movement.y = Input.GetAxisRaw("Vertical");
+        void Update()
+        {
+            _movement.x = Input.GetAxisRaw("Horizontal");
+            _movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", _movement.x);
-        animator.SetFloat("Vertical", _movement.y);
-        animator.SetFloat("Velocity", _movement.sqrMagnitude);
-    }
+            animator.SetFloat("Horizontal", _movement.x);
+            animator.SetFloat("Vertical", _movement.y);
+            animator.SetFloat("Velocity", _movement.sqrMagnitude);
+        }
 
-    void FixedUpdate()
-    {
-        Vector2 distanceToMove = _movement * (movementSpeedActual * Time.fixedDeltaTime);
-        rb.MovePosition(rb.position + distanceToMove);
+        void FixedUpdate()
+        {
+            Vector2 distanceToMove = _movement * (movementSpeedActual * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + distanceToMove);
+        }
     }
 }
