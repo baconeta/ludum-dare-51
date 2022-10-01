f7812335967a5f06c213eacb243b1d6f487777e6
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2022-10-01 17:36:53 +1300

    Add player movement and animation script

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    /*
+     * Player movement speed.
+     */
+    [Tooltip("How far in game units that the player can move per tick.")] [SerializeField]
+    public float movementSpeedInitial = 2F;
+    public float movementSpeedActual;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    private Vector2 _movement;
+
+    void Start()
+    {
+        movementSpeedActual = movementSpeedInitial;
+
+        animator.SetFloat("Horizontal", _movement.x);
+        animator.SetFloat("Vertical", _movement.y);
+        animator.SetFloat("Velocity", _movement.sqrMagnitude);
+    }
+
+    void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + ((movementSpeedActual * Time.fixedDeltaTime) * _movement));
+    }
+}
