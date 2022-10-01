ab10ac89f737f0e35b63379c6bd71bf6116dca4f
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-10-01 18:12:37 +0300

    prevent nullptr errors

--- Assets/Scripts/Collectables/Loot.cs
+++ Assets/Scripts/Collectables/Loot.cs
@@ -1,17 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace Collectables
 {
     // This could be a parent for the different types of loot that we can pickup, or it could be the end-all class.
     public class Loot : Collectable
     {
-        
         protected override void OnCollectablePickup()
         {
             //Give player value
-            player.GetPlayerStats().AddCurrency((int)value);
+            if (player == default)
+            {
+                player = FindObjectOfType<Player.Player>();
+            }
+
+            player.GetPlayerStats().AddCurrency((int) value);
             Destroy(gameObject);
         }
     }
