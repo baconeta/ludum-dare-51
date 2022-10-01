ac3d7669c166dcff11029a6682587514326e5093
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-10-02 02:17:59 +0300

    fix player collision and add game boundaries

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Player
@@ -7,8 +9,7 @@ namespace Player
         /*
      * Player movement speed.
      */
-        [Tooltip("How far in game units that the player can move per tick.")]
-        [SerializeField]
+        [Tooltip("How far in game units that the player can move per tick.")] [SerializeField]
         public float movementSpeedInitial = 2F;
 
         public float movementSpeedActual;
@@ -17,6 +18,22 @@ namespace Player
         public Animator animator;
 
         private Vector2 _movement;
+
+        [Header("Collision Handling")]
+        // A small offset (standoff) from colliders to ensure we don't try to get too close.
+        // Moving too close can mean we get hits when moving tangential to a surface which results
+        // in the controller not being able to move.
+        public float mContactOffset = 0.05f;
+
+        // A method to ensure we only iterate a certain number of times. Depending on the geometry,
+        // this can be increased but for most purposes 2 or 3 will suffice.
+        public int mMaxIterations = 2;
+
+        // Controls what the controller considers hits.
+        // Note: This allows us to control not only layers but potentially collision normal angles etc.
+        public ContactFilter2D mMovementFilter;
+
+        private List<RaycastHit2D> _mMovementHits = new(1);
 
         private void Start()
         {
@@ -36,15 +53,86 @@ namespace Player
 
                 animator.SetFloat("Horizontal", _movement.x);
                 animator.SetFloat("Vertical", _movement.y);
-                animator.SetFloat("Velocity", _movement.magnitude);
+                animator.SetFloat("Velocity", _movement.sqrMagnitude);
             }
-
         }
 
         void FixedUpdate()
         {
-            Vector2 distanceToMove = _movement * (movementSpeedActual * Time.fixedDeltaTime);
-            rb.MovePosition(rb.position + distanceToMove);
+            // Don't perform any work if no movement is required.
+            if (_movement.sqrMagnitude <= Mathf.Epsilon)
+                return;
+
+            // Grab the input movement unit direction.
+            var movementDirection = _movement.normalized;
+
+            // Calculate how much distance we'd like to cover this update.
+            var distanceRemaining = Mathf.Min(movementSpeedInitial * Time.fixedDeltaTime, _movement.magnitude);
+
+            var startPosition = rb.position;
+
+            var maxIterations = mMaxIterations;
+            // Iterate up to a capped iteration limit or until we have no distance to move or we've clamped the direction of motion to zero.
+            const float epsilon = 0.005f;
+            while (
+                maxIterations-- > 0 &&
+                distanceRemaining > epsilon &&
+                movementDirection.sqrMagnitude > epsilon
+            )
+            {
+                var distance = distanceRemaining;
+
+                // Perform a cast in the current movement direction using the colliders on the Rigidbody.
+                // Note: A potentially better way of doing this is to do an arbitrary shape cast such as Physics2D.CapsuleCast/BoxCast etc.
+                // At least when performing a specific shape query, we have no need to reposition the Rigidbody2D before each query.
+                var hitCount = rb.Cast(movementDirection, mMovementFilter, _mMovementHits, distance);
+
+                // Did we have any hits?
+                if (hitCount > 0)
+                {
+                    // Yes, so for this controller we're only interested in the first results which is the first hit.
+                    RaycastHit2D hit = _mMovementHits[0];
+
+                    // We're only interested in movement if it's beyond the contact offset.
+                    if (hit.distance > mContactOffset)
+                    {
+                        // Calculate the distance we'd like to move.
+                        distance = hit.distance - mContactOffset;
+
+                        // Reposition the Rigidbody2D to the hit point.
+                        // NOTE: Again, this can be avoided by a different choice of query.
+                        rb.position += movementDirection * distance;
+                    }
+                    else
+                    {
+                        // We had a hit but it resulted in us touching or being inside the contact offset.
+                        distance = 0f;
+                    }
+
+                    // Clamp the movement direction.
+                    // NOTE: This is effectively how we iterate and change direction for the queries.
+                    movementDirection -= hit.normal * Vector2.Dot(movementDirection, hit.normal);
+                }
+                else
+                {
+                    // No hit so move by the whole distance.
+                    rb.position += movementDirection * distance;
+                }
+
+                // Remove the distance we ended up moving from the remaining.
+                distanceRemaining -= distance;
+            }
+
+
+            // Reset the input movement.
+            _movement = Vector2.zero;
+
+            // Reset the Rigidbody2D position due to changes during querying.
+            // NOTE: We can avoid this setting of the Rigidbody2D position by a different choice of query.
+            var targetPosition = rb.position;
+            rb.position = startPosition;
+
+            rb.MovePosition(targetPosition);
         }
     }
 }
