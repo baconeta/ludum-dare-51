0c5191f091008d38801ee840a35487d197b36ee0
Holly <hollyducoing@gmail.com>
2022-10-01 17:59:40 +1300

    base code

---
+++ Assets/Scripts/ModeSwap/SpriteSwapper.cs
@@ -0,0 +1,33 @@
+﻿namespace DefaultNamespace;
+using UnityEngine;
+public class SpriteSwapper :MonoBehaviour
+{
+    public delegate void Swap(bool swap);
+    private Swap lightMode;
+    private Swap darkMode;
+    public void Start()
+    {
+        lightMode = true;
+        darkMode = false;
+
+    }
+
+    void SwitchToFalse(bool boolean)
+    {
+        boolean = false;
+    }
+
+    void SwitchToTrue(bool boolean)
+    {
+        boolean = true;
+    }
+
+    public void Update()
+    {
+        if (darkMode)
+        {
+            // sprites get switched out
+            // filter turns on
+        }
+    }
+}
