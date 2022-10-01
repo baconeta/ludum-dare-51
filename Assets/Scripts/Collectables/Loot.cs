4d800ce518c11a8f54676a91e863632ff0c7b14f
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2022-10-01 13:21:00 +1300

    Add stub files

---
+++ Assets/Scripts/Collectables/Loot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Collectables
+{
+    // This could be a parent for the different types of loot that we can pickup, or it could be the end-all class.
+    public class Loot : Collectable
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            /*
+             * This is a stub.
+             */
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            /*
+             * This is a stub.
+             */
+        }
+    }
+}
