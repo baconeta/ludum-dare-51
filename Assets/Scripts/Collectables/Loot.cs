498875fb3940dafccc1caf7b229d30ae54022378
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-10-02 16:48:47 +0300

    pickup volumes

--- Assets/Scripts/Collectables/Loot.cs
+++ Assets/Scripts/Collectables/Loot.cs
@@ -5,7 +5,6 @@ namespace Collectables
     // This could be a parent for the different types of loot that we can pickup, or it could be the end-all class.
     public class Loot : Collectable
     {
-        
         protected override void OnCollectablePickup()
         {
             //Give player value
@@ -13,9 +12,10 @@ namespace Collectables
             {
                 player = FindObjectOfType<Player.Player>();
             }
-            AudioSource.PlayClipAtPoint(pickupSound, transform.position);
-            
-            player.GetPlayerStats().AddCurrency((int) value);
+
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position, 0.15f);
+
+            player.GetPlayerStats().AddCurrency(value);
             Destroy(gameObject);
         }
     }
