e92378ee94f70bdee41b8c815125b7dcc5da3445
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-10-03 18:23:57 +0300

    add corn popping logic and sounds

--- Assets/Scripts/Objects/BossProjectile.cs
+++ Assets/Scripts/Objects/BossProjectile.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using Player;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using Random = UnityEngine.Random;
 
 namespace Objects
 {
@@ -11,6 +13,9 @@ namespace Objects
         public float distanceToStartPopping = 2f;
         private bool keepMoving;
         private bool popping;
+        private static readonly int Pop = Animator.StringToHash("Pop");
+        [SerializeField] private AudioClip[] popSounds;
+        [SerializeField] private AudioSource audioSource;
 
         public override void ShootTarget(Vector3 newTargetPosition, GameObject newSource, float newSpeed,
             float newDamage)
@@ -24,17 +29,24 @@ namespace Objects
             _damage = newDamage;
             keepMoving = true;
             popping = false;
+            GetComponent<CircleCollider2D>().enabled = false;
         }
 
         private void FixedUpdate()
         {
+            // TODO could make this way prettier with a curve oh well
             if (!keepMoving) return;
 
             Vector3 shootVector = _target - transform.position;
             if (!popping && shootVector.magnitude <= distanceToStartPopping)
             {
-                animator.SetTrigger("Pop");
+                animator.SetTrigger(Pop);
                 popping = true;
+                GetComponent<CircleCollider2D>().enabled = true;
+                foreach (AudioClip popSound in popSounds)
+                {
+                    StartCoroutine(PlayPop(popSound));
+                }
             }
 
             if (shootVector.magnitude <= 0.5f)
@@ -46,6 +58,12 @@ namespace Objects
                 _rigidbody2D.MovePosition(
                     Vector3.MoveTowards(transform.position, _target, _speed * Time.fixedDeltaTime));
             }
+        }
+
+        private IEnumerator PlayPop(AudioClip popSound)
+        {
+            yield return new WaitForSeconds(Random.Range(0f, 0.3f));
+            audioSource.PlayOneShot(popSound);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
